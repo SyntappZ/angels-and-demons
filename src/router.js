70da22be713a26753559873754980ad7a2eb2596
syntappz <doddsanater87@gmail.com>
2019-07-25 13:18:22 +0100

    setup

--- src/router.js
+++ src/router.js
@@ -14,12 +14,16 @@ export default new Router({
       component: Home
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (about.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+      path: '/angels',
+      name: 'angels',
+     
+      component: () => import('./views/Angels.vue')
+    },
+    {
+      path: '/demons',
+      name: 'demons',
+     
+      component: () => import('./views/Demons.vue')
     }
   ]
 })
