07157322432b13f6fd1905f7fc0eace4f107d8a3
syntappz <doddsanater87@gmail.com>
2019-07-26 12:37:46 +0100

    added angels and demons lists

---
+++ src/angels.js
@@ -0,0 +1,631 @@
+let angels = [
+  {
+    angel: "Abaddon, also known in Greek as Apollyon",
+    religion: "Christianity, Judaism",
+    type: "None",
+    domain: "Destruction"
+  },
+  {
+    angel: "Abathar Muzania",
+    religion: "Mandaeism",
+    type: "None",
+    domain: "The weighing of souls"
+  },
+  {
+    angel: "Anael, see Haniel",
+    religion: "Judaism",
+    type: "None",
+    domain: "Joy and Pleasure"
+  },
+  {
+    angel: "Arariel",
+    religion: "Jewish mythology",
+    type: "None",
+    domain: "Waters of the Earth"
+  },
+  {
+    angel: "Archangel (type)",
+    religion: "Christianity, Islam, Judaism",
+    type: "(type)",
+    domain: "None"
+  },
+  {
+    angel: "Ariel (angel)",
+    religion: "Christianity, Judaism",
+    type: "None",
+    domain: "Personification of Israel[citation needed]"
+  },
+  {
+    angel: "Artiya'il",
+    religion: "Islam",
+    type: "None",
+    domain: "Removes human grief"
+  },
+  {
+    angel: "Azazel",
+    religion: "Christianity, Islam, Judaism, Yazdânism",
+    type: "Archangel, fallen angel",
+    domain:
+      "teacher (for humans: of evil arts), leader of a group of angels, rebellion against God"
+  },
+  {
+    angel: "Azrael",
+    religion: "Christianity, Islam, Judaism",
+    type: "Archangel",
+    domain: "Death/Retribution"
+  },
+  {
+    angel: "Barachiel",
+    religion: "Christianity",
+    type: "Archangel, chief of the guardian angels",
+    domain: "Lightning; Blessings/Guardian Angels"
+  },
+  {
+    angel: "Baraqiel",
+    religion: "Jewish mythology",
+    type: "Archangel",
+    domain: "None"
+  },
+  {
+    angel: "Bene Elohim (type)",
+    religion: "Christianity, Judaism",
+    type: '"Sons of God" (type)',
+    domain: "None"
+  },
+  {
+    angel: "Camael, a.k.a. Kemuel or Kamael or Khamael",
+    religion: "Christianity, Judaism",
+    type: "Archangel, leader of the Powers, one of the Dominions",
+    domain: "Strength, Courage and War"
+  },
+  {
+    angel: "Cassiel",
+    religion: "Christianity, Islam, Judaism",
+    type: "Archangel",
+    domain: "Solitude and Tears"
+  },
+  {
+    angel: "Cherub/Cherubim (type)",
+    religion: "Christianity, Judaism, Islam",
+    type: "(type)",
+    domain: "None"
+  },
+  {
+    angel: "Cherubiel/Kerubiel",
+    religion: "Christianity, Judaism, Islam",
+    type: "cherub",
+    domain: "None"
+  },
+  {
+    angel: "Daniel",
+    religion: "Christianity, Judaism",
+    type: "Watcher",
+    domain: "None"
+  },
+  {
+    angel: "Dadrail",
+    religion: "Islam,[1] Yazdânism",
+    type: "Archangel (in Yazdanism)",
+    domain: "None"
+  },
+  {
+    angel: "Dominions a.k.a. Kyriotetes (type)",
+    religion: "Christianity, Judaism",
+    type: "(type)",
+    domain: "None"
+  },
+  {
+    angel: "Dumah",
+    religion: "Christianity, Islam, Judaism",
+    type: "None",
+    domain:
+      "Silence, vindication and the stillness of death, tormenting the wicked after death"
+  },
+  { angel: "Eleleth", religion: "Sethianism", type: "None", domain: "None" },
+  {
+    angel: "Eremiel/Jerahmeel",
+    religion: "Christianity, Judaism",
+    type: "None",
+    domain: "Watches/Guides the holy deceased in the afterlife"
+  },
+  {
+    angel: "Gabriel, also known in Arabic texts as Jibra'il",
+    religion: "Christianity, Judaism, Islam, Mormonism, Yazdânism",
+    type: "Archangel, one of the cherubim",
+    domain:
+      "Messengers, military (in Islam), destruction (in Judaism and Islam)"
+  },
+  {
+    angel: "Gadreel",
+    religion: "Christianity, Judaism",
+    type: "None",
+    domain: "None"
+  },
+  {
+    angel: "Grigori a.k.a. Watchers (type)",
+    religion: "Christianity, Judaism",
+    type: "(type)",
+    domain: "None"
+  },
+  {
+    angel: "Hadraniel",
+    religion: "Christianity, Judaism",
+    type: "None",
+    domain: "Second Heavenly Gate"
+  },
+  {
+    angel: "Hahasiah",
+    religion: "Christianity, Judaism",
+    type: "Principality",
+    domain: "None"
+  },
+  {
+    angel: "Haniel",
+    religion: "Christianity, Judaism",
+    type:
+      "Archangel, leader of the Principalities along with Archangel Netzach",
+    domain: "The Sephirah Netzach"
+  },
+  {
+    angel: "Harut",
+    religion: "Islam",
+    type: "Fallen angel",
+    domain: "Sorcery"
+  },
+  {
+    angel: "Hashmal[2]",
+    religion: "Christianity, Judaism",
+    type: "Dominions",
+    domain: "None"
+  },
+  { angel: "Hamalat al-Arsh", religion: "Islam", type: "None", domain: "None" },
+  {
+    angel: "Hesediel see also Zadkiel",
+    religion: "Christianity, Judaism",
+    type: "None",
+    domain: "Freedom, benevolence and mercy"
+  },
+  { angel: "Hofniel", religion: "Judaism", type: "Archangel", domain: "None" },
+  {
+    angel: "Imamiah",
+    religion: "Christianity, Judaism]",
+    type: "Principality",
+    domain: "None"
+  },
+  {
+    angel: "Ishim (type)",
+    religion: "Judaism, Islam",
+    type: "(type)",
+    domain: "None"
+  },
+  {
+    angel: "Israfil, also spelled Israfel, often considered same as Raphael",
+    religion: "Islam",
+    type: "Archangel",
+    domain: "Music"
+  },
+  {
+    angel: "Jegudiel",
+    religion: "Christianity",
+    type: "Archangel",
+    domain: "Responsibility and merciful love"
+  },
+  {
+    angel: "Jehoel[3]",
+    religion: "Christianity, Judaism",
+    type: "Seraph/Seraphim",
+    domain: "Fire"
+  },
+  {
+    angel: "Jequn[4]",
+    religion: "Christianity, Judaism",
+    type: "None",
+    domain: "None"
+  },
+  {
+    angel: "Jerahmeel",
+    religion: "Christianity, Judaism",
+    type: "Archangel",
+    domain: "None"
+  },
+  {
+    angel: "Jophiel",
+    religion: "Christianity, Judaism",
+    type: "Archangel",
+    domain: "Wisdom, Understanding, and Judgement"
+  },
+  {
+    angel: "Kalka'il",
+    religion: "Islam",
+    type: "None",
+    domain: "Fifth heaven"
+  },
+  {
+    angel: "Kepharel",
+    religion: "Jewish mythology",
+    type: "Archangel",
+    domain: "None"
+  },
+  { angel: "Kerubiel", religion: "Judaism", type: "None", domain: "None" },
+  {
+    angel: "Kiraman Katibin",
+    religion: "Islam",
+    type: "None",
+    domain: "Recorders of human thoughts, acts and feelings"
+  },
+  {
+    angel: "Kushiel",
+    religion: "Christianity, Judaism",
+    type: "None",
+    domain: "Punishment"
+  },
+  {
+    angel: "Leliel",
+    religion: "Christianity, Judaism",
+    type: "None",
+    domain: "Night"
+  },
+  {
+    angel: "Lucifer, also known in Hebrew as Helel",
+    religion: "Christianity",
+    type: "Archangel or seraph or cherub, fallen angel",
+    domain: "Bringer of Light"
+  },
+  { angel: "Maalik", religion: "Islam", type: "None", domain: "the Hellfire" },
+  {
+    angel: "Marut",
+    religion: "Islam",
+    type: "Fallen angel",
+    domain: "Sorcery"
+  },
+  {
+    angel: "Mebahiah",
+    religion: "Christianity, Judaism",
+    type: "Principality",
+    domain: "None"
+  },
+  {
+    angel: "Melek Taus",
+    religion: "Yazdânism",
+    type: "Archangel",
+    domain: "(Title)"
+  },
+  {
+    angel: "Metatron[5]",
+    religion: "Judaism, Christianity, Islam",
+    type: "Archangel, one of the seraph",
+    domain: "The Celestial Scribe"
+  },
+  {
+    angel: "Michael, also known in Arabic texts as Mikail",
+    religion: "Christianity, Judaism, Mormonism, Islam, Yazdânism",
+    type: "Archangel, one of the seraph[6]",
+    domain:
+      "The Military, Angel of Mercy (in Islam), General in God’s army. Angel of Death. (in Catholicism)"
+  },
+  {
+    angel: "Moroni",
+    religion: "Mormonism",
+    type: "None",
+    domain: "The Golden Plates"
+  },
+  {
+    angel: "Mu’aqqibat (type)",
+    religion: "Islam",
+    type: "(type)",
+    domain: "None"
+  },
+  {
+    angel: "Munkar",
+    religion: "Islam",
+    type: "None",
+    domain: "The Faith of the Dead"
+  },
+  {
+    angel: "Muriel",
+    religion: "Christianity",
+    type: "Dominions[7]",
+    domain: "June and Cancer in astrology"
+  },
+  {
+    angel: "Nakir",
+    religion: "Islam",
+    type: "None",
+    domain: "The Faith of the Dead"
+  },
+  {
+    angel: "Nanael",
+    religion: "Christianity, Judaism",
+    type: "Principality",
+    domain: "None"
+  },
+  {
+    angel: "Netzach",
+    religion: "Christianity, Judaism",
+    type: "Leader of the Principalities along with Archangel Haniel",
+    domain: "Eternity"
+  },
+  {
+    angel: "Nithael",
+    religion: "Christianity, Judaism",
+    type: "Principality",
+    domain: "None"
+  },
+  {
+    angel: "Nuriel",
+    religion: "Jewish mythology",
+    type: "None",
+    domain: "Hailstorms"
+  },
+  {
+    angel: "Ophaniel (Ofaniel, etc.)",
+    religion: "Christianity, Judaism",
+    type: "Cherubim; sometimes listed as one of the Thrones",
+    domain: "None"
+  },
+  {
+    angel: "Pahaliah",
+    religion: "Christianity",
+    type: "Thrones",
+    domain: "Virtuosity"
+  },
+  {
+    angel: "Penemue",
+    religion: "Christianity, Judaism",
+    type: "None",
+    domain: "None"
+  },
+  {
+    angel: "Phanuel",
+    religion: "Christianity, Judaism",
+    type: "Archangel",
+    domain: "Repentance and hope"
+  },
+  {
+    angel: "Powers (type)",
+    religion: "Christianity, Judaism",
+    type: "(type)",
+    domain: "None"
+  },
+  {
+    angel: "Poyel",
+    religion: "Christianity, Judaism",
+    type: "Principality",
+    domain: "None"
+  },
+  {
+    angel: "Pravuil",
+    religion: "Jewish mythology",
+    type: "Archangel",
+    domain: "God's scribe and record-keeper"
+  },
+  {
+    angel: "Principalities (type)",
+    religion: "Christianity, Judaism",
+    type: "(type)",
+    domain: "None"
+  },
+  {
+    angel: "Puriel",
+    religion: "Christianity, Judaism",
+    type: "None",
+    domain: "Examines the souls of those brought to heaven"
+  },
+  {
+    angel: "Qaphsiel",
+    religion: "Christianity, Judaism",
+    type: "None",
+    domain: "Angel of tears, temperance. Presides over the deaths of kings."
+  },
+  {
+    angel: "Radueriel",
+    religion: "Jewish mythology",
+    type: "None",
+    domain: "Can create lesser angels with a mere utterance"
+  },
+  {
+    angel: "Raguel, also known in Arabic texts as Azrael",
+    religion: "Christianity, Judaism, Islam",
+    type: "Archangel",
+    domain: "Angel of Justice"
+  },
+  {
+    angel: "Raphael, also known in Arabic texts as Israfel",
+    religion: "Christianity, Judaism, Islam, Mormonism, Yazdânism",
+    type: "Archangel, leader of the Virtues, one of the cherubim",
+    domain:
+      "in the Christian tradition, Raphael performs all manners of healing"
+  },
+  {
+    angel: "Raziel",
+    religion: "Christianity, Judaism",
+    type: "Archangel",
+    domain: "Keeper of Secrets"
+  },
+  {
+    angel: "Remiel",
+    religion: "Christianity, Judaism",
+    type: "Archangel",
+    domain: "None"
+  },
+  {
+    angel: "Rikbiel",
+    religion: "Christianity, Judaism",
+    type: "Cherubim",
+    domain: "None"
+  },
+  {
+    angel: "Sachiel",
+    religion: "Christianity, Judaism",
+    type: "Archangel, Cherub",
+    domain: "Wealth and Charity"
+  },
+  {
+    angel: "Sahaquiel",
+    religion: "Jewish mythology",
+    type: "Archangel",
+    domain: "Guardian of the fourth heaven"
+  },
+  {
+    angel: "Sabriel",
+    religion: "Jewish mythology",
+    type: "Archangel",
+    domain: "Miracles"
+  },
+  {
+    angel: "Samael",
+    religion: "Christianity, Judaism",
+    type: "Archangel",
+    domain: "Death and fetching souls"
+  },
+  {
+    angel: "Sandalphon",
+    religion: "Christianity, Judaism",
+    type: "Archangel",
+    domain:
+      'protector of unborn children (some sources: "twin brother" of Metatron)'
+  },
+  {
+    angel: "Sarathiel",
+    religion: "Christianity",
+    type: "Archangel",
+    domain: "Discipline and Penance"
+  },
+  {
+    angel: "Sariel",
+    religion: "Christianity, Judaism, Islam",
+    type: "Archangel",
+    domain: "None"
+  },
+  {
+    angel: "Schemhampharae",
+    religion: "Christianity, Judaism",
+    type: "None",
+    domain:
+      "A list of 72 angels of the 9 choir orders, with esoteric meaning related to the names of God"
+  },
+  {
+    angel: "Selaphiel",
+    religion: "Christianity",
+    type: "Archangel",
+    domain: "Patron saint of prayer and worship"
+  },
+  {
+    angel: "Seraph/Seraphim (type)",
+    religion: "Christianity, Judaism",
+    type: "(type)",
+    domain: "None"
+  },
+  {
+    angel: "Seraphiel[8]",
+    religion: "Christianity, Judaism",
+    type: "seraph",
+    domain: "Protector of Metatron, highest ranking saraphim"
+  },
+  {
+    angel: "Shamnail",
+    religion: "Yazdânism",
+    type: "Archangel",
+    domain: "None"
+  },
+  {
+    angel: "Shamsiel",
+    religion: "Christianity, Judaism",
+    type: "None",
+    domain: "None"
+  },
+  {
+    angel: "Sidriel",
+    religion: "Jewish mythology",
+    type: "Archangel",
+    domain: "None"
+  },
+  {
+    angel: "Song-Uttering Choirs (type)",
+    religion: "Judaism",
+    type: "(type)",
+    domain: "None"
+  },
+  {
+    angel: "Temeluchus",
+    religion: "Christianity, Judaism",
+    type: "None",
+    domain: "None"
+  },
+  {
+    angel: "Tennin",
+    religion: "Japanese mythology",
+    type: "None",
+    domain: "None"
+  },
+  {
+    angel: "Thrones (type)",
+    religion: "Christianity, Judaism",
+    type: "(type)",
+    domain: "None"
+  },
+  { angel: "Turail", religion: "Yazdânism", type: "Archangel", domain: "None" },
+  {
+    angel: "Uriel",
+    religion: "Christianity, Judaism",
+    type: "Archangel, one of the seraphim[9]",
+    domain: '"El/God is my light"; patron of the Arts'
+  },
+  { angel: "Uziel", religion: "Judaism", type: "Archangel", domain: "None" },
+  {
+    angel: "Vasiariah[10]",
+    religion: "Christianity, Judaism",
+    type: "Dominions",
+    domain: "None"
+  },
+  {
+    angel: "Vehuel",
+    religion: "Christianity, Judaism",
+    type: "Principality",
+    domain: "None"
+  },
+  {
+    angel: "Virtues (type)",
+    religion: "Christianity, Judaism",
+    type: "(type)",
+    domain: "None"
+  },
+  { angel: "Wormwood", religion: "Christianity", type: "None", domain: "war" },
+  {
+    angel: "Zachariel[11]",
+    religion: "Christianity",
+    type: "Archangel",
+    domain: "None"
+  },
+  {
+    angel: "Zadkiel a.k.a. Tzadkiel[11]",
+    religion: "Christianity, Judaism",
+    type: "Archangel, leader of the Dominions",
+    domain:
+      '"Righteousness of God" archangel of freedom, benevolence, mercy, and the Patron Angel of all who forgive'
+  },
+  {
+    angel: "Zaphkiel a.k.a. Tzaphkiel, Tzaphqiel",
+    religion: "Christianity, Judaism",
+    type: "Archangel, leader of the Thrones",
+    domain: 'name means "God\'s knowledge"'
+  },
+  {
+    angel: "Zephaniel",
+    religion: "Judaism",
+    type: "Archangel",
+    domain: "None"
+  },
+  {
+    angel: "Zephon",
+    religion: "Jewish mythology",
+    type: "None",
+    domain: "None"
+  },
+  {
+    angel: "Zophiel",
+    religion: "Christianity, Judaism",
+    type: "Cherubim",
+    domain: "None"
+  }
+];
